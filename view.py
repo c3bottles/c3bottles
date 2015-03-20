031c13a7555f4258e07cc1ae507953f38aeb8e39
MichiK <michik+github@michik.net>
2015-03-20 15:53:50 +0100

    Add route stubs, improve dp list
    The drop point list now shows the last reported state. This has to be
    improved visually. Table layout now depends on the device size. View
    adapted so it gives last state and orders drop points by priority
    and number.
    
    Some route stubs are now present in view, which will later contain
    other pages.

--- view.py
+++ view.py
@@ -10,16 +10,46 @@ def index():
 def faq():
 	return render_template("faq.html")
 
+@c3bottles.route("/numbers")
+def statistics():
+	return "TODO: Show some nice statistics"
+
 @c3bottles.route("/list")
 def dp_list():
 	from model import DropPoint
 	all_dps = []
 	for dp in db.session.query(DropPoint).order_by(DropPoint.number).all():
+		if dp.get_new_reports():
+			last_state = dp.get_new_reports()[0].state
+		else:
+			last_state = None
 		all_dps.append({
 			"number": dp.number,
 			"location": dp.get_current_location(),
 			"reports_total": dp.get_total_report_count(),
 			"reports_new": dp.get_new_report_count(),
+			"priority": dp.get_priority(),
+			"last_state": last_state,
 			"removed": True if dp.removed else False
 			})
-	return render_template("list.html", all_dps=all_dps)
+	return render_template(
+		"list.html",
+		all_dps=sorted(all_dps, key=lambda k: k["priority"], reverse=True)
+		)
+
+@c3bottles.route("/map")
+def dp_map():
+	return "TODO: Show the drop point map"
+
+@c3bottles.route("/view/<int:dp_number>")
+def dp_view(dp_number):
+	return "TODO: View details of drop point " + str(dp_number)
+
+@c3bottles.route("/report/<int:dp_number>")
+@c3bottles.route("/<int:dp_number>")
+def dp_report(dp_number):
+	return "TODO: Report drop point " + str(dp_number)
+
+@c3bottles.route("/visit/<int:dp_number>")
+def dp_visit(dp_number):
+	return "TODO: Visit drop point " + str(dp_number)
