b9f0813afc604287b43bf157268f49e01cf804c2
MichiK <michik+github@michik.net>
2015-03-23 16:38:32 +0100

    Add modals for drop point reporting and visiting
    The list now has icons for every drop point which open modals allowing the
    user to report or log a visit of the respective drop point. The buttons
    are not functional yet. A third modal showing brief details of the drop
    point has yet to be implemented.

--- static/js/c3bottles.js
+++ static/js/c3bottles.js
@@ -1,16 +1,25 @@
-$('body').on('click', 'a[data-toggle=collapse]', function() {
+$("body").on("click", "a[data-toggle=collapse]", function() {
 	var _self = $(this);
 	var _group = _self.parent().parent().parent();
-	var _button = _group.children('button');
-	var _target = $(_self.attr('data-target'));
-	if (_target.hasClass('in')) {
-		_self.parent().removeClass('active');
+	var _button = _group.children("button");
+	var _target = $(_self.attr("data-target"));
+	if (_target.hasClass("in")) {
+		_self.parent().removeClass("active");
 	} else {
-		_self.parent().addClass('active');
+		_self.parent().addClass("active");
 	}
 	if (_group.find("li.active").length > 0) {
 		_button.removeClass("btn-default").addClass("btn-primary");
 	} else {
 		_button.removeClass("btn-primary").addClass("btn-default");
 	}
+});
+
+$(".dp_modal").on("show.bs.modal", function() {
+	var _details = $(this).data("bs.modal").options.details;
+	$(".modal_dp_id").text(_details.number);
+});
+
+$(".dp_modal").on("hidden.bs.modal", function () {
+	$(this).removeData("bs.modal");
 });
