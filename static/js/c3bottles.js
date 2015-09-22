307e7b0df05b220339abb6d0fe335b5286c156f6
MichiK <michik+github@michik.net>
2015-09-22 16:40:10 +0200

    Add some JS comments

--- static/js/c3bottles.js
+++ static/js/c3bottles.js
@@ -1,3 +1,12 @@
+/*
+ * Add an event handler to the buttons over the drop point table in the list
+ * to toggle table rows based on classes for the location or state of the
+ * drop points.
+ *
+ * On a click, this function not only toggles the visibility of the table rows
+ * affected but also adds/removes classes to the buttons accordingly,
+ *
+ */
 $("body").on("click", "a[data-toggle=collapse]", function () {
     var _self = $(this);
     var _group = _self.parent().parent().parent();
@@ -15,6 +24,14 @@ $("body").on("click", "a[data-toggle=collapse]", function () {
     }
 });
 
+/*
+ * When showing any of the drop point modals (details, reporting, visiting),
+ * all elements displaying details of the drop point in question are filled
+ * with the correct details before the modal is made visible to the user.
+ *
+ * All the details are read from the drop point table.
+ *
+ */
 $(".dp_modal").on("show.bs.modal", function (e) {
     var _tr = $(e.relatedTarget).parent().parent();
     var _details = _tr.data("details");
@@ -24,10 +41,21 @@ $(".dp_modal").on("show.bs.modal", function (e) {
     $("#modal_dp_link").attr("href", _tr.data("href"));
 });
 
+/*
+ * When hiding any of the drop point modals (details, reporting, visiting),
+ * the modal must be destroyed to be re-constructed on the next show instead
+ * of simply hiding it and displaying the same instance again.
+ *
+ */
 $(".dp_modal").on("hidden.bs.modal", function () {
     $(this).removeData("bs.modal");
 });
 
+/*
+ * This dict contains all the states for drop points which can be reported. The
+ * states are mapped to classes of buttons triggering the respective report.
+ *
+ */
 var report_states = {
     ".report-default": "DEFAULT",
     ".report-nocrates": "NO_CRATES",
@@ -38,12 +66,24 @@ var report_states = {
     ".report-overflow": "OVERFLOW"
 };
 
+/*
+ * Add an event handler to all the report buttons.
+ *
+ */
 $.each(report_states, function (cls, state) {
     $(cls).on("click", function () {
         report_dp($(".modal_dp_number").first().text(), state);
     });
 });
 
+/*
+ * Report a drop point through the AJAX API.
+ *
+ * The state of the drop point in question is sent through the API and the
+ * modal with the buttons is hidden. After the API request has been finished,
+ * an alert indicating success or failure is displayed.
+ *
+ */
 function report_dp(num, state) {
     $("#dp_report_modal").modal("hide");
     $.ajax({
@@ -83,7 +123,12 @@ function report_dp(num, state) {
         }
     });
 }
-
+/*
+ * This dict contains all actions which can be performed by the bottle
+ * collection team when visiting a drop point. The actions are mapped to the
+ * classes of buttons triggering the respective action.
+ *
+ */
 var visit_actions = {
     ".visit-emptied": "EMPTIED",
     ".visit-addedcrates": "ADDED_CRATE",
@@ -93,12 +138,23 @@ var visit_actions = {
     ".visit-noaction": "NO_ACTION"
 };
 
+/*
+ * Add an event handler to all the visit buttons.
+ */
 $.each(visit_actions, function (cls, action) {
     $(cls).on("click", function () {
         visit_dp($(".modal_dp_number").first().text(), action);
     });
 });
 
+/*
+ * Log the visit of a drop point through the AJAX API.
+ *
+ * The action performed when visiting the drop point is sent through the API
+ * and the modal with the buttons is hidden. After the API request has been
+ * finished, an alert indicating success or failure is displayed.
+ *
+ */
 function visit_dp(num, action) {
     $("#dp_visit_modal").modal("hide");
     $.ajax({
