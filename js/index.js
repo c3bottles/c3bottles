fed4fabdae479c91d0a96785aefbfba1afaddcb0
marudor <marudor@marudor.de>
2017-12-25 12:55:11 +0100

    fix datatables, use webpack for modern stuff

---
+++ js/index.js
@@ -0,0 +1,9 @@
+require('./areas');
+require('./create_dp');
+require('./dp_modals');
+require('./drop_points');
+require('./gettext');
+require('./list');
+require('./main');
+require('./map');
+require('./stats');
