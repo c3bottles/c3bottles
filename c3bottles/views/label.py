9ba458c03604e7d546d5561a262a3dc8bf548305
marudor <marudor@marudor.de>
2018-12-27 21:02:11 +0100

    Allow specific label for a category

--- c3bottles/views/label.py
+++ c3bottles/views/label.py
@@ -3,11 +3,13 @@ from base64 import b64encode
 from cairosvg import svg2pdf
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from io import BytesIO
+from stringcase import snakecase, lowercase
 
-from flask import Blueprint, render_template, request, Response
+from flask import Blueprint, render_template, request, Response, abort
 
 from c3bottles import app
 from c3bottles.model.drop_point import DropPoint
+from c3bottles.model.category import Category
 from c3bottles.views import needs_visiting
 
 
@@ -17,8 +19,10 @@ bp = Blueprint("label", __name__)
 @bp.route("/label/<int:number>.pdf")
 @needs_visiting
 def for_dp(number):
-    DropPoint.query.get_or_404(number)
-    return Response(_create_pdf(number), mimetype="application/pdf")
+    dp = DropPoint.query.get(number)
+    if (dp is None):
+        return abort(404)
+    return Response(_create_pdf(dp), mimetype="application/pdf")
 
 
 @bp.route("/label/all.pdf")
@@ -26,7 +30,7 @@ def for_dp(number):
 def all_labels():
     output = PdfFileWriter()
     for dp in DropPoint.query.filter(DropPoint.removed == None).all():  # noqa
-        output.addPage(PdfFileReader(BytesIO(_create_pdf(dp.number))).getPage(0))
+        output.addPage(PdfFileReader(BytesIO(_create_pdf(dp))).getPage(0))
     f = BytesIO()
     output.write(f)
     return Response(
@@ -35,10 +39,32 @@ def all_labels():
     )
 
 
-def _create_pdf(number):
-    img = qrcode.make(request.url_root + str(number))
+@bp.route("/label/category/<int:number>.pdf")
+@needs_visiting
+def for_cat(number):
+    cat = Category.get(number)
+    if (cat is None):
+        return abort(404)
+    output = PdfFileWriter()
+    for dp in DropPoint.query.filter(DropPoint.category_id == cat.category_id, DropPoint.removed == None).all():  # noqa
+        output.addPage(PdfFileReader(BytesIO(_create_pdf(dp))).getPage(0))
+    f = BytesIO()
+    output.write(f)
+    return Response(
+        f.getvalue(),
+        mimetype="application/pdf"
+    )
+
+
+def _create_pdf(dp: DropPoint):
+    img = qrcode.make(request.url_root + str(dp.number))
     f = BytesIO()
     img.save(f)
     b64 = b64encode(f.getvalue()).decode("utf-8")
     label_style = app.config.get("LABEL_STYLE", "default")
-    return svg2pdf(render_template("label/{}.svg".format(label_style), number=number, qr=b64))
+    specific_label_style = label_style + "_" + snakecase(lowercase(dp.category.name))
+    print(specific_label_style, label_style)
+    try:
+        return svg2pdf(render_template("label/{}.svg".format(specific_label_style), number=dp.number, qr=b64))  # noqa
+    except:  # noqa
+        return svg2pdf(render_template("label/{}.svg".format(label_style), number=dp.number, qr=b64))  # noqa
