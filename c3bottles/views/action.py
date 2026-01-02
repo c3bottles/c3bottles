b6f064f1b8ee61aa7bb529bd018d1980dd34337a
Johann150 <johann.galle@protonmail.com>
2026-01-02 19:08:32 +0100

    prevent safe HTTP method from sending a report
    fixes https://github.com/c3bottles/c3bottles/issues/181

--- c3bottles/views/action.py
+++ c3bottles/views/action.py
@@ -20,7 +20,7 @@ def report(number: int = None):
 
     state = request.values.get("state")
 
-    if state:
+    if state and request.method != "GET":
         try:
             Report(dp=dp, state=state)
         except ValueError as e:
